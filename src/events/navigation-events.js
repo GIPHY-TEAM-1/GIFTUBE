e7afcfc4033de205e757c6d38845f07d6f6f926a
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-01-17 19:51:47 +0200

    Implementing navigation events

---
+++ src/events/navigation-events.js
@@ -0,0 +1,46 @@
+import { q, setActiveNav } from './helpers.js';
+import { ABOUT, CONTAINER_SELECTOR, FAVORITES, TRENDING } from '../common/constants.js';
+import { loadTrending } from '../requests/request-service.js'
+import { toTrendingView } from '../views/trending-view.js'
+import { toAboutView } from '../views/about-view.js'
+import { toHomeView } from '../views/home-view.js'
+import { HOME } from '../common/constants.js';
+
+export const loadPage = (page = '') => {
+    switch (page) {
+
+        case HOME:
+            setActiveNav(HOME);
+            return renderHome();
+
+        case TRENDING:
+            setActiveNav(TRENDING);
+            return renderTrending();
+
+        case FAVORITES:
+            setActiveNav(FAVORITES);
+            return renderFavorites();
+
+        case ABOUT:
+            setActiveNav(ABOUT);
+            return renderAbout();
+
+        default: return null;
+    }
+};
+
+const renderHome =  () => {
+    q(CONTAINER_SELECTOR).innerHTML = toHomeView();
+}
+
+const renderTrending = async () => {
+    q(CONTAINER_SELECTOR).innerHTML = toTrendingView(await loadTrending());
+}
+
+const renderFavorites = () => {
+    q(CONTAINER_SELECTOR).innerHTML = toFavoritesView(getFavorites());
+}
+
+const renderAbout = () => {
+    q(CONTAINER_SELECTOR).innerHTML = toAboutView();
+};
