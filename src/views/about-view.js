e9046b166b4e571f159185142e0a1e7877d3b013
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-01-17 19:53:48 +0200

    Adding views

---
+++ src/views/about-view.js
@@ -0,0 +1,12 @@
+export const toAboutView = () => `
+    <div id="about">
+        <div class="content">
+            <p>Step into the vibrant world of GIFtube, your ultimate destination for the hottest and most trending GIFs on the internet. Curated for your enjoyment, GIFtube is your go-to hub for the latest and greatest animated moments that are setting the digital world on fire.</p>
+            <p>Our journey began with a simple mission – to bring you a carefully curated selection of the most buzzworthy GIFs that capture the pulse of the online community. From viral sensations to hilarious memes, heartwarming clips, and everything in between, GIFtube is your front-row seat to the dynamic and ever-evolving world of internet culture.</p>
+            <p>Here at GIFtube, we understand that the internet moves at the speed of a click, and trends come and go in the blink of an eye. That's why our team works tirelessly to handpick the most exciting and share-worthy GIFs, ensuring that you're always in the loop and ready to be part of the latest online conversations.</p>
+            <p>Join our community of GIF enthusiasts and let GIFtube be your daily dose of internet culture. Whether you're looking to share a laugh, express your feelings, or simply stay on top of what's trending, GIFtube is the place to be.</p>
+            <p>Get ready to discover, share, and immerse yourself in the world of trending GIFs at GIFtube – where every upload tells a story, and the internet's most exciting moments come to life.</p>
+        </div>
+    </div>
+
+`
---
+++ src/views/home-view.js
@@ -0,0 +1,13 @@
+export const toHomeView = () => `
+    <div id="home-view">
+        <h1>Gifs App</h1>
+        <div class = "content">
+            <p>This is gifs app. You can:</p>
+            <ul>
+                <li>See trending gifs</li>
+                <li>Search for gifs</li>
+                <li>Save your favorite gifs</li>
+            </ul>
+        </div>
+    </div>
+`
