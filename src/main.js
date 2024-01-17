30de068aab763a6f9e5ba1afa118a15d3e411678
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-01-17 19:54:46 +0200

    Modifying main.js

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { HOME } from "./common/constants.js";
+import { loadPage } from "./events/navigation-events.js";
+import { q } from "./events/helpers.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM loaded');
+    
+    document.addEventListener('click', (event) => {
+        if(event.target.classList.contains('nav-link')) {
+            loadPage(event.target.getAttribute('data-page'));
+        }
+    });
+
+     loadPage(HOME);
+    
+});
