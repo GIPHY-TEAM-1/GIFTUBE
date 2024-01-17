eea1f4827aa4946d9bf75bb28d4530bc8dd78809
Georgi Popov <gwvr028@gmail.com>
2024-01-17 22:57:55 +0200

    Refactor fetch functions
    Removed useless async/await syntax from the fetch functions. They will be returning a promise insted, which will later be awaited in the impelementing functions.

--- src/common/constants.js
+++ src/common/constants.js
@@ -12,8 +12,8 @@ export const UPLOAD = 'upload';
 
 export const APIKey = '8HymYwpltTZGDtB4oK7t5Te7t617IfM6';
 
-export const trendingURL = async (limit = 25, offset = 0) => await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${APIKey}&limit=${limit}&offset=${offset}&rating=g`); 
-
 export const CONTAINER_SELECTOR = '#container';
 
-export const searchURL = async (query = '', limit = 25, offset = 0) => await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${query}&limit=${limit}&offset=${offset}&rating=g&lang=en`);
+export const trendingURL = (limit = 25, offset = 0) => fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${APIKey}&limit=${limit}&offset=${offset}&rating=g`);
+
+export const searchURL = (query = '', limit = 25, offset = 0) => fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${query}&limit=${limit}&offset=${offset}&rating=g&lang=en`);
