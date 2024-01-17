6bb803dace09439bf78df23c7378e895b1de27bc
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-01-17 19:48:40 +0200

    Implementing trending-data

---
+++ src/data/trending-data.js
@@ -0,0 +1,16 @@
+import {trendingURL} from '../common/constants.js';
+
+export const getTrending = async () => {
+    const response = await trendingURL();
+    const json = await response.json();
+    console.log(json);
+    return json.data.map(element => {
+        console.log(element.url);
+        return {
+            id: element.id,
+            username: element.username,
+            title: element.title,
+            image: element.images.fixed_width.url
+        }
+    })
+};
