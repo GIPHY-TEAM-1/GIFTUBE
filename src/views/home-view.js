17f7dec0ced39ebcd6e8f2588a6b943496893814
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-01-18 06:49:32 +0200

    replacing home-view content

--- src/views/home-view.js
+++ src/views/home-view.js
@@ -1,12 +1,22 @@
 export const toHomeView = () => `
     <div id="home-view">
-        <h1>Gifs App</h1>
+        <!-- <h1>GITtube App</h1> --!>
+
+        <h1>Welcome to GIFTube App: The GIFs We "Borrowed" Edition!</h1>
         <div class = "content">
-            <p>This is gifs app. You can:</p>
+
+            <!-- <p>This is gifs app. You can:</p>
             <ul>
                 <li>See trending gifs</li>
                 <li>Search for gifs</li>
                 <li>Save your favorite gifs</li>
+            </ul> --!>
+
+            <h3>What you can do on GIFTube App:</h3> 
+            <ul>
+              <li>See the latest and greatest stolen... oops, we mean "carefully curated" GIFs.</li>
+              <li>Search for GIFs that may or may not have slipped through the cracks of copyright law.</li>
+              <li>Save your favorites because, let's be honest, they're probably already saved on your camera roll.</li>
             </ul>
         </div>
     </div>
