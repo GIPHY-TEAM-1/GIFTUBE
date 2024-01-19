7c53bb1acb6baad1ef455feaab3b21f9509232e7
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-01-19 14:37:14 +0200

    getGifById & randomGif added in constants.js

--- src/common/constants.js
+++ src/common/constants.js
@@ -20,9 +20,14 @@ export const trendingURL = (limit = 25, offset = 0) => fetch(`https://api.giphy.
 
 export const searchURL = (query = '', limit = 25, offset = 0) => fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${query}&limit=${limit}&offset=${offset}&rating=g&lang=en`);
 
-export const emptyHeart = `<span class="material-symbols-outlined favorite">favorite</span>`;
+export const getGifById = (gifId) => fetch(`https://api.giphy.com/v1/gifs/${gifId}?api_key=8HymYwpltTZGDtB4oK7t5Te7t617IfM6&rating=g`);
 
-export const fullHeart = `<span class="material-symbols-outlined favorite active">favorite</span>
+export const randomGif = fetch('https://api.giphy.com/v1/gifs/random?api_key=8HymYwpltTZGDtB4oK7t5Te7t617IfM6&tag=&rating=g');
+
+// To check emptyHeart
+export const emptyHeart = `<span class="material-symbols-outlined favorite">favorite</span>`;
+// To check fullHeart
+export const fullHeart = (gifId) => `<span class="material-symbols-outlined favorite active" id: ${gifId}>favorite</span>
     <style>
     #full-heart {
       font-variation-settings:
