6b6a7b1e32d5364f3334315d093283397d9cc7b1
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-01-19 04:34:08 +0200

    Create empty and full heart for favorites

--- src/common/constants.js
+++ src/common/constants.js
@@ -19,3 +19,22 @@ export const GIF_CLICK = 'gif-click';
 export const trendingURL = (limit = 25, offset = 0) => fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${APIKey}&limit=${limit}&offset=${offset}&rating=g`);
 
 export const searchURL = (query = '', limit = 25, offset = 0) => fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&q=${query}&limit=${limit}&offset=${offset}&rating=g&lang=en`);
+
+export const emptyHeart = (gifId) => {
+    return `<span class="material-symbols-outlined favorite" data-gif-id=${gifId}>favorite</span>`
+};
+
+export const fullHeart = (gifId) => {
+    return `<span class="material-symbols-outlined favorite active" data-gif-id=${gifId}>favorite</span>
+    <style>
+    #full-heart {
+      font-variation-settings:
+      'FILL' 1,
+      'wght' 400,
+      'GRAD' 0,
+      'opsz' 24
+    }
+    </style>`
+};
+
+export const link = `<span class="material-symbols-outlined link-btn">link</span>`;
