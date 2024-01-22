0856433fb2648498b2c7366be40d3684b04f5041
Georgi Popov <gwvr028@gmail.com>
2024-01-23 00:50:23 +0200

    Refactor for styling

--- src/views/upload-btn-view.js
+++ src/views/upload-btn-view.js
@@ -1,13 +1,15 @@
 export const toUploadBtnView = () => {
     return `
-    <h1>GIFTUBE Upload</h1>
-    <p>Upload your collection to share on a <a href="https://giphy.com/">giphy.com</a></p>
-    <form id="form-upload">
-        <label for="file">File</label>
-        <input id="file-input" type="file" accept=".gif" />
-        </br></br>
-        <button class="upload-button" id="inner-upload-btn">Upload</button>
-    </form>
-    `
-}
-
+    <div class="upload-btn-view gif-view">
+        <h3><strong>GIFTUBE</strong> Upload</h3>
+        <div class="upload-btn-container">
+            <p>Upload your own gifs to share on <a href="https://giphy.com/">giphy.com</a></p>
+            <form id="form-upload">
+                <label for="file-input">Choose file</label>
+                <input id="file-input" type="file" accept=".gif" />
+                <button class="upload-form-button" id="inner-upload-btn">Upload</button>
+            </form>
+        </div>
+    </div>
+    `;
+};
